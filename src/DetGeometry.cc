d8c0da6d7316c012aa3cde154dc884c33a8bf83f
Filchenkov <ifkf@mail.ru>
2017-03-11 10:51:01 +0300

    res

--- src/DetGeometry.cc
+++ src/DetGeometry.cc
@@ -26,17 +26,20 @@ G4VPhysicalVolume* DetGeometry::Construct(){
     G4Material* p_material = nist->FindOrBuildMaterial("G4_ALANINE");
     G4Material* t_material = nist->FindOrBuildMaterial("G4_ANTHRACENE");
 
-    G4Cons* con = new G4Cons ("conus", 20 *cm, 40 *cm, 10 *cm, 20 *cm, 50 *cm, 0, pi);
-    G4Tubs* tube = new G4Tubs ("tube", 20 *cm, 40 *cm, 50 *cm, 0, pi/2);
-    G4Para* para = new G4Para ("para", 20 *cm, 20 *cm, 20 *cm, pi/3, pi/3, pi/3);
+//G4Cons* con = new G4Cons ("conus", 20 *cm, 40 *cm, 10 *cm, 20 *cm, 50 *cm, 0, pi);
+    G4Tubs* tube = new G4Tubs ("tube", 0 *cm, 40 *cm, 150 *cm, 0, pi*2);
+    G4Tubs* res = new G4Tubs ("tube", 0 *cm, 20 *cm, 100 *cm, 0, pi*2);
+    //G4Para* para = new G4Para ("para", 20 *cm, 20 *cm, 20 *cm, pi/3, pi/3, pi/3);
 
-    G4LogicalVolume* conus2 = new G4LogicalVolume (con, c_material, "mat_cone");
+   // G4LogicalVolume* conus2 = new G4LogicalVolume (con, c_material, "mat_cone");
     G4LogicalVolume* tube2 = new G4LogicalVolume (tube, c_material, "mat_tube");
-    G4LogicalVolume* para2 = new G4LogicalVolume (para, c_material, "mat_para");
+    G4LogicalVolume* res2 = new G4LogicalVolume (res, c_material, "mat_tube");
+   // G4LogicalVolume* para2 = new G4LogicalVolume (para, c_material, "mat_para");
 
-    G4VPhysicalVolume* conus = new G4PVPlacement (0, G4ThreeVector(-250 *cm,0,0), conus2, "cons1",logicWorld,false,0);
-    G4VPhysicalVolume* tubes = new G4PVPlacement (0, G4ThreeVector(0,250 *cm,500 *cm), tube2, "tube1",logicWorld,false,0);
-    G4VPhysicalVolume* paras = new G4PVPlacement (0, G4ThreeVector(250 *cm,500*cm,0), para2, "para1",logicWorld,false,0);
+  //  G4VPhysicalVolume* conus = new G4PVPlacement (0, G4ThreeVector(-250 *cm,0,0), conus2, "cons1",logicWorld,false,0);
+    G4VPhysicalVolume* tubes = new G4PVPlacement (0, G4ThreeVector(), tube2, "tube1",logicWorld,false,0);
+    G4VPhysicalVolume* res3 = new G4PVPlacement (0, G4ThreeVector(0,250 *cm,500 *cm), res2, "tube1",logicWorld,false,0);
+   // G4VPhysicalVolume* paras = new G4PVPlacement (0, G4ThreeVector(250 *cm,500*cm,0), para2, "para1",logicWorld,false,0);
 
 
     return physWorld;
